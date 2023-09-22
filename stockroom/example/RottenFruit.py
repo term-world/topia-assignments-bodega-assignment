2bec097ebde0c4c38eec6532abc6e6cfee5ccc95
dluman <dluman@allegheny.edu>
2023-09-21 20:56:51 -0400

    Updating with narrator path/grader fixes

--- stockroom/example/RottenFruit.py
+++ stockroom/example/RottenFruit.py
@@ -2,7 +2,5 @@ from inventory.Item import ItemSpec
 
 class RottenFruit(ItemSpec):
 
-  consumable = True
-
-  def __init__(self):
-    super().__init__(__file__)
+    def __init__(self):
+        super().__init__(__file__)
